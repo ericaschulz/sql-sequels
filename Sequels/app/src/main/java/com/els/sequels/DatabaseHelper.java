0ae781f186e800f591dffa311511c2b461dcc2b8
ericaschulz <ericalynnschulz@gmail.com>
2016-06-22 11:11:26 -0500

    Update

---
+++ Sequels/app/src/main/java/com/els/sequels/DatabaseHelper.java
@@ -0,0 +1,27 @@
+package com.els.sequels;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by ericaschulz on 6/22/16.
+ */
+public abstract class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final int DB_VERSION = 1; //change as we upgrade;
+    public static final String DB_NAME = "ReallyBadSequels.db";
+    public DatabaseHelper(Context context) {
+        super(context,DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
